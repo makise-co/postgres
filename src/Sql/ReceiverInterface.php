4355fb07520ac6c923c1cd2c47f69bc33ea38e22
Dmitry K <coder1994@gmail.com>
2020-05-19 23:28:41 +0300

    Fix double copyright block

--- src/Sql/ReceiverInterface.php
+++ src/Sql/ReceiverInterface.php
@@ -6,13 +6,6 @@
  * (c) Dmitry K. <coder1994@gmail.com>
  */
 
-/**
- * This file is part of the Makise-Co Postgres Client
- * World line: 0.571024a
- *
- * (c) Dmitry K. <coder1994@gmail.com>
- */
-
 declare(strict_types=1);
 
 namespace MakiseCo\Postgres\Sql;
